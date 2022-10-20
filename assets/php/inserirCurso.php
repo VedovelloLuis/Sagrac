8aa354e4e7984d5c3ed38fed634eefd06da528f7
Luis27225 <luvedbr@gmail.com>
2022-10-20 16:44:49 -0300

    Adição do Back-End Insertion e alterações no Front

--- assets/php/inserirCurso.php
+++ assets/php/inserirCurso.php
@@ -1,21 +1,55 @@
-<?php 
- if (isset($_POST['submit'])) {
+<?php
+$cursonome = $_POST["name"];
+$chtotal = $_POST["carga"];
+$cursoid = $_POST["id"];
 
-    print_r('Nome do Curso:' . $_POST['nome']);
-    print_r('<br>');
-    print_r('Carga Horária Total:' . $_POST['carga']);
-    print_r('<br>');
+$servername = "localhost";
+$database = "bd_atribuicoesdisciplinares";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Conexão não feita." . mysqli_connect_error());
+}
 
- }
+else{
 
-//     include_once('config.php');
-//     $cursonome = $_POST['inputCursoTurma'];
-//     $chtotal = $_POST['inputChTotal'];
+    echo "Conectado com sucesso ao banco de dados <br>";
     
-//     $result = $mysqli_query($conexao, "INSERT INTO tab_curso(cursoTurma, chTotal) 
-//     VALUES ($cursonome, $chtotal)");
+
+
+$sql = "INSERT INTO tab_curso (cursoTurma, cursoId, chTotal, periodo) VALUES ('$cursonome','$cursoid','$chtotal', 'Tarde')";
+
+
 
  
 
+if (mysqli_query($conn, $sql)) {
+      echo "Nova inserção criada com sucesso <br>";
 
+
+
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+      }
 ?>
+
+
+
+<html>
+<body>
+INSERIDO: <br>
+Id do curso: <?php echo $_POST["id"]; ?><br>
+Nome do curso: <?php echo $_POST["name"]; ?><br>
+Carga horária total: <?php echo $_POST["carga"]; ?> horas.
+
+
+<a href="../../project/diretorInserirCurso.html"> Voltar e inserir outro curso</a>
+
+
+</body>
+</html>
